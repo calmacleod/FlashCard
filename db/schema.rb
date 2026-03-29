4771d1356b0bbfbcc1344d08c90f1a217c61d29e
commvnist <commvnist@proton.me>
2026-03-29 13:22:57 -0400

    fixing budget/thinking + migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_25_000002) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_29_171919) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -125,7 +125,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_25_000002) do
     t.datetime "created_at", null: false
     t.integer "message_id", null: false
     t.string "name", null: false
-    t.string "thought_signature"
+    t.text "thought_signature"
     t.string "tool_call_id", null: false
     t.datetime "updated_at", null: false
     t.index ["message_id"], name: "index_tool_calls_on_message_id"
